2082df656d4221f8836951ee00e41a26f9580d07
jiawei <zhongjiawei@huyi.cn>
2017-11-29 11:31:31 +0800

    Initial Commit

---
+++ PickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickerView
+//
+//  Created by jw on 2017/11/29.
+//  Copyright © 2017年 jw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
